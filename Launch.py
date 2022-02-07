487f2d712c9484da731278c0d46c2b1ce32dec04
mark <89848850+wottreng@users.noreply.github.com>
2022-02-07 09:59:42 -0500

    Add files via upload

---
+++ Launch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#/usr/local/bin/python3
+
+import os
+curdir = os.getcwd()
+os.system(curdir+'/.a/ProjectSoftwareLaunch.py')
+#file1=curdir+'/.a/ProjectSoftwareLaunch.py'
+
+#import files.ProjectSoftwareLaunch
+#print('hello')
